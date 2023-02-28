8f43f76df7724a0d45881f2dbd72f4b59d4498e8
ktonge <karltonge9@gmail.com>
2023-02-28 17:01:02 -0500

    fixing mistakes update

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -89,9 +89,9 @@ with open(file_to_save, "w") as txt_file:
     txt_file.write(election_results)
 
     # 6a: Write a for loop to get the county from the county dictionary
-    for county_name in county_votes:
+    for county_name in county_options:
         # 6b: Retrieve the county vote count.
-        total_county_vote = county_votes[county_name]
+        total_county_votes = county_votes[county_name]
         # 6c: Calculate the percentage of votes for the county.
         county_vote_percentage = float(total_county_votes)/float(total_votes) *100
          # 6d: Print the county results to the terminal.
@@ -101,7 +101,7 @@ with open(file_to_save, "w") as txt_file:
          # 6e: Save the county votes to a text file.
         txt_file.write(county_results)
          # 6f: Write an if statement to determine the winning county and get its vote count.
-        if (total_county_votes>largest_county):
+        if (total_county_votes>largest_voter_turnout):
             largest_county=county_name
             largest_voter_turnout=total_county_votes
     
